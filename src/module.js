b234ca06cd1a207a0d8558e749ac6245d8375d6e
meek0 <cedricgfont@gmail.com>
2022-06-27 12:38:09 -0400

    can be built with no errors

--- src/module.js
+++ src/module.js
@@ -1,4 +1,4 @@
-angular.module('sfObibaSelectionTree', ['schemaForm', 'sfObibaSimpleMdeTemplates', 'ngObiba'])
+angular.module('sfObibaSelectionTree', ['schemaForm', 'sfObibaSelectionTreeTemplates', 'ngObiba'])
 .config(['schemaFormProvider', 'schemaFormDecoratorsProvider', 'sfBuilderProvider', 'sfPathProvider',
   function (schemaFormProvider, schemaFormDecoratorsProvider, sfBuilderProvider, sfPathProvider) {
 
